84547981a1d9e4f371e849554a4ca8c05c2766ab
JensBrandels <brandelsjens@gmail.com>
2025-04-03 20:02:00 +0200

    fixed so sass can be used more smoothly

--- packages/pages/home/ui/index.tsx
+++ packages/pages/home/ui/index.tsx
@@ -1,9 +1,10 @@
+import "./style.scss";
 import { Link } from "react-router-dom";
 
 export const Home = () => {
   return (
-    <div>
-      <h1>Welcome to Viking Survivors</h1>
+    <div className="home-container">
+      <h1 className="home-title">Welcome to Viking Survivors</h1>
       <Link to="/selectionscreen">Login</Link>
     </div>
   );
